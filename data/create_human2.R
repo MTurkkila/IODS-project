ed494302112b54314165dab1c628c626e4c948b2
Turkkila Miikka V P <miturkki@lx8-500-032.pc.helsinki.fi>
2020-11-20 12:48:16 +0200

    ch5 data wrangling done

---
+++ data/create_human2.R
@@ -0,0 +1,20 @@
+# Data wrangling exercise 5 (Dimensionality reduction techniques)
+# Miikka Turkkila, 20.11.2020
+
+# Load "human" data
+human <- read.csv("data/human.csv")
+
+# exclude unneeded varaibles
+
+
+# Removing rows with missing data/values
+
+
+# Remove observations relating to regions
+
+# Countries as rownames
+
+# check dimension
+dim(human)
+
+# Save the data (overwriting the old file)
